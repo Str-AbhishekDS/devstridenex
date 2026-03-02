509a98950499d2c8dbad42d46715103e9b3dbb3f
Abhishek Sadale <abhishek.sadale@erpdata.in>
2026-03-02 12:58:04 +0530

    2 march- reduced api for diffrent docktype

--- stridenex_app/api_stridenex_app/college/master.py
+++ stridenex_app/api_stridenex_app/college/master.py
@@ -6,50 +6,25 @@ from stridenex_app.api_stridenex_app.app_utils import (
     ) 
 
 
-"""List of all state"""
-
 @frappe.whitelist(allow_guest=True)
-def get_state():
-    states = frappe.get_all("State", fields=["state_name"])
-    if states:
-        gen_response(200, "State list retrieved successfully", states)
-    else:
-        gen_response(400, "No data found", {"success": False})
+def get_master_data(doctype=None, filters=None, fields=None):
 
-"""List of all district as per state"""
+    if not doctype:
+        return gen_response(400, "DocType is required", {"success": False})
 
-@frappe.whitelist(allow_guest=True)
-def get_district(state=None):
-    if not state:
-        return gen_response(400, "State is required", {"success": False})
+    try:
+        if not filters:
+            filters = {}
 
-    districts = frappe.get_all(
-        "District", filters={"state": state}, fields=["district_name"]
-    )
-    if districts:
-        return gen_response(200, "District list retrieved successfully", districts)
-    else:
-        return gen_response(404, "No data found", [])
+        if not fields:
+            fields = ["name"]
 
+        data = frappe.get_all(doctype, filters=filters, fields=fields)
 
-"""list of tehsil as per district"""
+        if data:
+            return gen_response(200, f"{doctype} list retrieved successfully", data)
+        else:
+            return gen_response(404, "No data found", [])
 
-@frappe.whitelist(allow_guest=True)
-def get_tahsil(district=None):
-    state_names = frappe.get_all(
-    "Tahsil", filters={"district": district}, fields=["tahsil_name"]
-    )
-    if state_names:
-        gen_response(200, "Tahsil list retrieved successfully", state_names)
-    else:
-        gen_response(400, "No data found", {"success": False})
-
-@frappe.whitelist(allow_guest=True)
-def get_city(district=None, tahsil=None):
-    state_names = frappe.get_all(
-    "City", filters={"district": district, "tahsil": tahsil}, fields=["city_name"]
-    )
-    if state_names:
-        gen_response(200, "City list retrieved successfully", state_names)
-    else:
-        gen_response(400, "No data found", {"success": False})
+    except Exception as e:
+        return gen_response(500, str(e), {"success": False})
