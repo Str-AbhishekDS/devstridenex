f2b7602d7b9e5a50a3730fe126c77534a61e97e7
Abhishek Sadale <abhishek.sadale@erpdata.in>
2026-03-09 10:04:49 +0530

    9/3/2026: College onbording Email Send After Approve

--- stridenex_app/stridenex_app/doctype/college/college.py
+++ stridenex_app/stridenex_app/doctype/college/college.py
@@ -1,9 +1,30 @@
 # Copyright (c) 2026, QTPL and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class College(Document):
-	pass
+
+    def on_update_after_submit(self):
+
+        # Check if status changed to Approved
+        if self.approved == "Approved" and self.get_doc_before_save().approved != "Approved":
+
+            frappe.sendmail(
+                recipients=[self.email],
+                subject="Welcome to Our Platform",
+                message=f"""
+                Dear {self.college_name},
+
+                Congratulations!
+
+                Your college registration has been approved.
+
+                Welcome to our platform.
+
+                Regards,
+                Team
+                """
+            )
